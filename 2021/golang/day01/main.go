14b638aa5f6b60c360a42b59de15a1e9b2e2a8ee
Marco Chomut <marco.chomut@gmail.com>
2022-01-04 15:50:45 -0500

    Add rest of naive day01 solutions

--- 2021/golang/day01/main.go
+++ 2021/golang/day01/main.go
@@ -1,11 +1,60 @@
 package main
 
 import (
+    "bufio"
+    "flag"
     "fmt"
+    "log"
+    "os"
 )
 
 func main() {
-    fmt.Println("letz heck thes")
+    livePtr := flag.Bool("live", false, "whether to fetch the input live from the internet")
+    // TODO: usage instructions for args: single path to input file
+    // TODO: also allow input to come via stdin, to use curl pipe
+    flag.Parse()
+    live := *livePtr
+    filePaths := flag.Args()
+
+    if live {
+        if len(filePaths) > 0 {
+            log.Fatalf("invalid args, may only fetch input live or supply file arg, not both: %s", filePaths)
+        }
+    } else {
+        if len(filePaths) == 0 {
+            log.Fatal("invalid args, expected one input file path, found none")
+        } else if len(filePaths) > 1 {
+            log.Fatalf("invalid args, expected only one input file path, found multiple: %s", filePaths)
+        }
+    }
+
+    if live {
+        // TODO: write me
+        return
+    }
+
+    f, err := os.Open(filePaths[0])
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
+
+    scanner := bufio.NewScanner(f)
+    depths := make([]int, 0)
+    var i int
+    for scanner.Scan() {
+        fmt.Sscanf(scanner.Text(), "%d", &i)
+        depths = append(depths, i)
+    }
+	if err := scanner.Err(); err != nil {
+        log.Fatal("could not read input:", err)
+	}
+
+    count := depthIncreaseCount(depths)
+    slidingCount := slidingDepthIncreaseCount(depths)
+
+    fmt.Println("part one:", count)
+    fmt.Println("part two:", slidingCount)
 }
 
 func depthIncreaseCount(depths []int) (count int) {
@@ -14,15 +63,54 @@ func depthIncreaseCount(depths []int) (count int) {
     for i, depth := range depths {
         // first depth doesn't count as an increase
         if i == 0 {
-            prevDepth = depth
-            continue
+            goto loop
         }
 
         if depth > prevDepth {
             count++
         }
 
+loop:
         prevDepth = depth
+    }
+
+    return
+}
+
+func slidingDepthIncreaseCount(depths []int) (count int) {
+    windowLen := 3
+    window := make([]int, windowLen)
+    var windowDepth int
+    var prevWindowDepth int
+
+    for i, depth := range depths {
+        if len(window) == windowLen {
+            _, window = window[0], window[1:]
+        }
+
+        window = append(window, depth)
+
+        // need a full window first
+        if i < (windowLen - 1) {
+            continue
+        }
+
+        windowDepth = 0
+        for _, d := range window {
+            windowDepth += d
+        }
+
+        // first window doesn't count as an increase
+        if i == (windowLen - 1) {
+            goto loop
+        }
+
+        if windowDepth > prevWindowDepth {
+            count++
+        }
+
+loop:
+        prevWindowDepth = windowDepth
     }
 
     return
--- 2021/golang/day01/main_test.go
+++ 2021/golang/day01/main_test.go
@@ -40,3 +40,54 @@ func TestDepthIncreaseCount(t *testing.T) {
         t.Errorf("only decreasing: expected 0 actual %d", actual)
     }
 }
+
+func TestSlidingDepthIncreaseCount(t *testing.T) {
+    depths := []int{1, 2, 3, 4, 5}
+    actual := slidingDepthIncreaseCount(depths)
+
+    if 2 != actual {
+        t.Errorf("simple: expected 2 actual %d", actual)
+    }
+
+    depths = []int{0, 3, 2, 1, 4}
+    actual = slidingDepthIncreaseCount(depths)
+
+    if 2 != actual {
+        t.Errorf("complex: expected 2 actual %d", actual)
+    }
+
+    depths = []int{}
+    actual = slidingDepthIncreaseCount(depths)
+
+    if 0 != actual {
+        t.Errorf("empty: expected 0 actual %d", actual)
+    }
+
+    depths = []int{1, 2, 3}
+    actual = slidingDepthIncreaseCount(depths)
+
+    if 0 != actual {
+        t.Errorf("single window: expected 0 actual %d", actual)
+    }
+
+    depths = []int{5, 4, 3, 2, 1}
+    actual = slidingDepthIncreaseCount(depths)
+
+    if 0 != actual {
+        t.Errorf("only decreasing: expected 0 actual %d", actual)
+    }
+
+    depths = []int{0, 2, 2, 1, 0}
+    actual = slidingDepthIncreaseCount(depths)
+
+    if 1 != actual {
+        t.Errorf("varying: expected 2 actual %d", actual)
+    }
+
+    depths = []int{1, 3, 1, 1, 3}
+    actual = slidingDepthIncreaseCount(depths)
+
+    if 0 != actual {
+        t.Errorf("constant: expected 0 actual %d", actual)
+    }
+}
