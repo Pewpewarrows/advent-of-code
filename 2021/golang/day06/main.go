0a5836b4167516dc149842e415b9fe9e5b8a6902
Marco Chomut <marco.chomut@gmail.com>
2022-01-10 17:05:34 -0500

    Add day06p2 solution

--- 2021/golang/day06/main.go
+++ 2021/golang/day06/main.go
@@ -15,9 +15,13 @@ func main() {
     var fishSchool []int
     advent.Execute(scanInputData, &fishSchool)
 
-    solution := simulatingLanternfishPopulation(fishSchool, 80)
+    // TODO: really have to figure out why the sim func mutates fishSchool
+    // solution := simulatingLanternfishPopulation(fishSchool, 80)
+    solution := estimatingLanternfishPopulation(fishSchool, 80)
+    fmt.Println("part one:", solution)
 
-    fmt.Println("solution:", solution)
+    solution = estimatingLanternfishPopulation(fishSchool, 256)
+    fmt.Println("part two:", solution)
 }
 
 func scanInputData(scanner *bufio.Scanner, inputDataPtr interface{}) {
@@ -56,6 +60,46 @@ func simulatingLanternfishPopulation(fishSchool []int, dayCount int) (fishCount 
     }
 
     fishCount = len(fishSchool)
+
+    return
+}
+
+// TODO: consider making fishCount be a big type?
+func estimatingLanternfishPopulation(fishSchool []int, dayCount int) (fishCount int) {
+    // this closed formula attempt didn't pan out...
+    // fmt.Println(dayCount % birthTimer)
+    // startingTimer := fishSchool[0]
+    // birthCycleCount := (dayCount - startingTimer - 1) / birthTimer
+    // if (dayCount > startingTimer) {
+    //     // account for the first birth
+    //     birthCycleCount++
+    // }
+
+    fishCountsByTimer := make(map[int]int)
+    var largestTimer int
+
+    for _, fish := range fishSchool {
+        fishCountsByTimer[fish]++
+
+        if fish > largestTimer {
+            largestTimer = fish
+        }
+    }
+
+    largestTimer = advent.MaxInt(largestTimer, startingBirthTimer)
+
+    for day := 0; day < dayCount; day++ {
+        pendingNewFish := fishCountsByTimer[0]
+        for i := 1; i <= largestTimer + 1; i++ {
+            fishCountsByTimer[i - 1] = fishCountsByTimer[i]
+        }
+        fishCountsByTimer[birthTimer - 1] += pendingNewFish
+        fishCountsByTimer[startingBirthTimer] += pendingNewFish
+    }
+
+    for _, f := range fishCountsByTimer {
+        fishCount += f
+    }
 
     return
 }
--- 2021/golang/day06/main_test.go
+++ 2021/golang/day06/main_test.go
@@ -11,4 +11,12 @@ func TestSimulatingLanternfishPopulation(t *testing.T) {
     if actual != 5934  {
         t.Errorf("simple: expected 5934 actual %d", actual)
     }
+
+    // TODO: why did fishSchool get modified above?
+    fishSchool = []int{3, 4, 3, 1, 2}
+    actual = estimatingLanternfishPopulation(fishSchool, 256)
+
+    if actual != 26984457539  {
+        t.Errorf("simple: expected 26984457539 actual %d", actual)
+    }
 }
